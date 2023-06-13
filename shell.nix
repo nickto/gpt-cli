131fac94385eecf6e347378ddc3220187a0e7cd9
Nick To <nick@nickto.net>
2023-06-13 10:55:16 +0300

    Add Nix set up for development

---
+++ shell.nix
@@ -0,0 +1,18 @@
+with ( import (builtins.fetchTarball https://channels.nixos.org/nixpkgs-23.05-darwin/nixexprs.tar.xz) {});
+mkShell {
+  buildInputs = [
+    poetry
+    python310
+  ];
+  shellHook = ''
+    # Set up virtual environment
+    if [ ! -d .venv ]; then
+        echo "No .venv found, creating..."
+        python -m venv .venv
+    fi;
+    source .venv/bin/activate
+
+    export LD_LIBRARY_PATH=${pkgs.zlib}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+  '';
+}
